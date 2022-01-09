cb21767d2793329c5bc4692d7c806bfc9fdb50dc
AmoreiraT <thiago.m.hq@gmail.com>
2022-01-09 04:22:09 -0300

    assets

---
+++ src/assets/images/svg/BlueminiIcon.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable max-len */
+import * as React from 'react';
+
+function Blueminiicon(props) {
+  return (
+    <svg
+      width={25}
+      height={26}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M5.002 13l.023 12h4.571l.082-12H5.002z"
+        stroke="#FBFDFF"
+        strokeMiterlimit={10}
+      />
+      <path
+        d="M5.002 13l.023 12h4.571l.082-12H5.002z"
+        stroke="#6750A4"
+        strokeOpacity={0.05}
+        strokeMiterlimit={10}
+      />
+      <path d="M9.762 13H5V1h4.762v12z" fill="#FBFDFF" />
+      <path d="M9.762 13H5V1h4.762v12z" fill="#6750A4" fillOpacity={0.05} />
+      <path
+        d="M9.762 13H5V1h4.762v12z"
+        stroke="#FBFDFF"
+        strokeMiterlimit={10}
+      />
+      <path
+        d="M9.762 13H5V1h4.762v12z"
+        stroke="#6750A4"
+        strokeOpacity={0.05}
+        strokeMiterlimit={10}
+      />
+      <path
+        d="M12.048 15.364A2.361 2.361 0 019.684 13H5a7.048 7.048 0 0014.095 0h-4.684a2.361 2.361 0 01-2.363 2.364z"
+        fill="#FBFDFF"
+      />
+      <path
+        d="M12.048 15.364A2.361 2.361 0 019.684 13H5a7.048 7.048 0 0014.095 0h-4.684a2.361 2.361 0 01-2.363 2.364z"
+        fill="#6750A4"
+        fillOpacity={0.05}
+      />
+      <path
+        d="M12.048 15.364A2.361 2.361 0 019.684 13H5a7.048 7.048 0 0014.095 0h-4.684a2.361 2.361 0 01-2.363 2.364z"
+        stroke="#FBFDFF"
+        strokeMiterlimit={10}
+      />
+      <path
+        d="M12.048 15.364A2.361 2.361 0 019.684 13H5a7.048 7.048 0 0014.095 0h-4.684a2.361 2.361 0 01-2.363 2.364z"
+        stroke="#6750A4"
+        strokeOpacity={0.05}
+        strokeMiterlimit={10}
+      />
+      <path
+        d="M12.048 10.636A2.361 2.361 0 009.684 13l-.09 12H5.023L5 13a7.048 7.048 0 0114.095 0h-4.684a2.361 2.361 0 00-2.363-2.364v0z"
+        stroke="#FBFDFF"
+        strokeMiterlimit={10}
+      />
+      <path
+        d="M12.048 10.636A2.361 2.361 0 009.684 13l-.09 12H5.023L5 13a7.048 7.048 0 0114.095 0h-4.684a2.361 2.361 0 00-2.363-2.364v0z"
+        stroke="#6750A4"
+        strokeOpacity={0.05}
+        strokeMiterlimit={10}
+      />
+    </svg>
+  );
+}
+
+export default Blueminiicon;
